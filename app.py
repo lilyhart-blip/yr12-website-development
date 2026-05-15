c3a1ade39a693ea7e73c0a826bd66b21fb03f8ae
lilyhart-blip <lilyhart@shgcham.school.nz>
2026-05-15 14:59:17 +1200

    added more css

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/Post')
+def index():
+    return render_template('post.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
 
