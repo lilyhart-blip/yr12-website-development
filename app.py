105b6174f1f0414c940c84e8e6f7bcd4e43cbfe6
lilyhart-blip <lilyhart@shgcham.school.nz>
2026-06-29 09:59:16 +1200

    end of class commit started dynamic routing

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, session, redirect
 import sqlite3
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "SuperSecret67"
 
 DATABASE = "app.db"
 
@@ -17,6 +19,7 @@ def query_db(sql,args=(),one=False):
 
 @app.route('/')
 def index():
+    results = query_db("SELECT * FROM snake") 
     return render_template('index.html')
 
 #adding in second route
@@ -36,22 +39,18 @@ def Signup():
     # if it is, then get data from request
     # Then actually send that data to the DB
     # Probably redirect or give user some 'toast' as success - flash
-        flash("Sign up Succsessful")
+            flash("Sign up Succsessful")
         return render_template('signup.html')
 
 @app.route('/signuppage')
 def signuppage():
     return render_template('signuppage.html')
 
-def query_db(sql, args=(), one=False):
-    '''connect and query- will retun one item if one=true and can accept arguments as tuple'''
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    cursor.execute(sql, args)
-    results = cursor.fetchall()
-    db.commit()
-    db.close()
-    return (results[0] if results else None) if one else results
+@app.route('/snake/<int:id>')
+def snake(id):
+    sql = "SELECT * FROM snake WHERE id=?"
+    snake = query_db(sql,args=(id,),one=True)
+    return render_template('Snakes.html', snake=snake)
 
 if __name__ == "__main__":
     app.run(debug=True)
